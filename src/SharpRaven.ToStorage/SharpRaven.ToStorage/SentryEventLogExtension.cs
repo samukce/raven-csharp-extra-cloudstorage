6de94df0abc0ebbd8285e599b308ed4e4a287d48
Samuel P <samuel.p.araujo@gmail.com>
2017-11-05 19:42:52 -0300

    Fixed typo.

--- src/SharpRaven.ToStorage/SharpRaven.ToStorage/SentryEventLogExtension.cs
+++ src/SharpRaven.ToStorage/SharpRaven.ToStorage/SentryEventLogExtension.cs
@@ -2,7 +2,7 @@ using SharpRaven.Data;
 
 namespace SharpRaven.ToStorage {
     public static class SentryEventLogExtension {
-        public static IConfigurationContainer WithCredenctials(this SentryEvent sentryEvent, string accountname, string keyValue) {
+        public static IConfigurationContainer WithCredentials(this SentryEvent sentryEvent, string accountname, string keyValue) {
             return new ConfigurationContainer(sentryEvent, accountname, keyValue);
         }
     }
